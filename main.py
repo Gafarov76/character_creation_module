1042db6f2c67644fdce53f87cd49581235e6ecc0
Max Gafarov <gafarovmakc@gmail.com>
2022-11-01 20:39:24 +0300

    111

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ DEFAULT_STAMINA = 80
 
 class Character:
     BREIF_DESC_CHAR_CLASS = 'отважный любитель приключений'
-    RANGE_VALUE_ATTACK = (0, 3)
+    RANGE_VALUE_ATTACK = (1, 3)
     RANGE_VALUE_DEFENCE = (1, 5)
     SPECIAL_BUFF = 15
     SPECIAL_SKILL = 'Удача'
@@ -67,11 +67,6 @@ class Healer(Character):
     SPECIAL_SKILL = 'Защита'
 
 
-
-
-
-
-
 def attack(char_name: str, char_class: str) -> str:
     """Функция атаки. Реализована с использованием случайного числа,
     генерируемого импортируемой функцией.
@@ -83,82 +78,50 @@ def attack(char_name: str, char_class: str) -> str:
         return f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}'
     if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику',
-                'равный {5 + randint(-3, -1)}')
+                f'равный {5 + randint(-3, -1)}')
 
-'''
-def defence(char_name: str, char_class: str) -> str:
-    """Функция защиты. Реализована с использованием случайного числа,
-    генерируемого импортируемой функцией.
+
+def start_training(character):
     """
-    if char_class == 'warrior':
-        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
-    if char_class == 'mage':
-        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
-    if char_class == 'healer':
-        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
-
-
-def special(char_name: str, char_class: str) -> str:
-    """Функция определяющая специальные навыки персонажа.
-    Добавляет дополнительные очки к характеристикам.
+    Принимает на вход имя и класс персонажа.
+    Возвращает сообщения о результатах цикла тренировки персонажа.
     """
-    if char_class == 'warrior':
-        return (f'{char_name} применил специальное ',
-                f'умение «Выносливость {80 + 25}»')
-    if char_class == 'mage':
-        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
-    if char_class == 'healer':
-        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    commands_classes = {'attack': character.attack, 'defence': character.defence, 
+                        'special': character.special}
 
-
-def start_training(char_name: str, char_class: str) -> str:
-    """Функция тренировки выбранного персонажа.
-    Имеет три варианта действий (атака, защита и специальное умение).
-    """
-    if char_class == 'warrior':
-        print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
-    if char_class == 'mage':
-        print(f'{char_name}, ты Маг — превосходный укротитель стихий.')
-    if char_class == 'healer':
-        print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
     print('Потренируйся управлять своими навыками.')
-    print('Введи одну из команд: attack — чтобы атаковать противника, ',
-          'defence — чтобы блокировать атаку противника или ',
+    print('Введи одну из команд: attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: None = None
+
+    cmd = None
+
     while cmd != 'skip':
-        cmd: str = input('Введи команду: ')
-        if cmd == 'attack':
-            print(attack(char_name, char_class))
-        if cmd == 'defence':
-            print(defence(char_name, char_class))
-        if cmd == 'special':
-            print(special(char_name, char_class))
+        cmd = input('Введи команду: ')
+
+        if cmd in commands_classes:
+            print(commands_classes[cmd]())
+       
     return 'Тренировка окончена.'
 
-
-def choice_char_class() -> str:
-    """Функция выбора имени и персонажа."""
-    approve_choice: str = None
-    char_class: str = None
+    
+def choice_char_class(char_name: str) -> Character:
+    # Добавили словарь, в котором соотносится ввод пользователя и класс персонажа.
+    game_classes = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
+    
+    approve_choice: str  = None
+    
     while approve_choice != 'y':
-        char_class = input((
-            'Введи название персонажа, ',
-            'за которого хочешь играть: ',
-            'Воитель — warrior, Маг — mage, Лекарь — healer: '))
-        if char_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя. ',
-                  'Сильный, выносливый и отважный.')
-        if char_class == 'mage':
-            print('Маг — находчивый воин дальнего боя. ',
-                  'Обладает высоким интеллектом.')
-        if char_class == 'healer':
-            print('Лекарь — могущественный заклинатель. ',
-                  'Черпает силы из природы, веры и духов.')
-        approve_choice = input(('Нажми (Y), чтобы подтвердить выбор, ',
-                                'или любую другую кнопку, ',
-                                'чтобы выбрать другого персонажа ')).lower()
+        selected_class = input('Введи название персонажа, '
+                           'за которого хочешь играть: Воитель — warrior, '
+                           'Маг — mage, Лекарь — healer: ')
+        char_class: Character = game_classes[selected_class](char_name)
+        # Вывели в терминал описание персонажа.
+        print(char_class)
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
+                               'или любую другую кнопку, '
+                               'чтобы выбрать другого персонажа ').lower()
     return char_class
 
 
@@ -171,9 +134,9 @@ if __name__ == '__main__':
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
-'''
+    char_class: Character = choice_char_class(char_name)
+    print(start_training(char_class))
+
 warrior = Warrior('Кодослав')
 print(warrior)
 print(warrior.attack())
